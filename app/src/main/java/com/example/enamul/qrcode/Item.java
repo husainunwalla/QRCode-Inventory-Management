dcd90215a50e6249ff91afa24a76924526717a4a
husainunwalla <mr.husainunwalla.com>
2020-03-22 19:31:26 +0530

    Item Class

---
+++ app/src/main/java/com/example/enamul/qrcode/Item.java
@@ -0,0 +1,22 @@
+package com.example.enamul.qrcode;
+
+public class Item {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    private String name;
+    private double price;
+}
